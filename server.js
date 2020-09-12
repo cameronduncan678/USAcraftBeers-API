856ac75fe143bba4522c5c93b01034073ff1ca87
Cameron Duncan <cameronduncan678@gmail.com>
2020-09-12 14:57:38 +0100

    dotenv config added

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const dotEnv = require('dotenv');
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = 9001;
-const NODE_ENV = 'Development';
+dotEnv.config({ path: './config/config.env' });
+
+const PORT = process.env.PORT;
+const NODE_ENV = process.env.NODE_ENV;
 
 app.use('/api', require('./Routes/GETmeta'));
 
